e94572d4d8396ece986d496dc5df02ad47db767a
Salmonacid1 <78323292+Salmonacid1@users.noreply.github.com>
2021-04-25 15:35:27 -0400

    Wildfires in 2020 Figure Created

--- Load_Data.m
+++ Load_Data.m
@@ -1,43 +1,66 @@
-%% Load Data
+%% Loading Data for Acres Burned due to Wildfires in California
 
-%Load 1987-2018 data; plot time vs acres burned graph 
 filename='1987-2018CalWildfireData';
-TotalAcresBurned=readtable(filename)
-Total_Burned= table2array (TotalAcresBurned)
-Total_Burned(9,2)=209815
+TotalAcresBurned=readtable(filename);
+Total_Burned=table2array(TotalAcresBurned);
+Total_Burned(9,2)=209815; %had to fill in value from table because comma caused it to be a NaN
 
-%% Graphing Exploratory 
-
-%plot(Total_Burned(:,1),Total_Burned(:,2), "markersize", 3)
-
+%Graphing Exploratory 
 figure (1)
 bar(Total_Burned(:,1),Total_Burned(:,2))
 xlabel("Year")
 ylabel("Acres Burned")
-title("Wildfires Acres Burned from 1987-2018")
+title("Acres Burned due to Wildfires from 1987-2018")
 
-%% wildfire 2020
-%%loading filename for 2020 wildfire california
+%% Loading Spatial Data for 2020 Wildfires in California
+
 filename='fire_nrt_M-C61_195333';
 WildfireLocations2020=readtable(filename);
-LAT = WildfireLocations2020.latitude;
-LON = WildfireLocations2020.longitude;
-%%removing lat/lon not in CA
-toDelete =LAT >42;
-LAT(toDelete, :) = [];
-toDelete = LAT<32;
-LAT(toDelete, :) = [];
-toDelete =LON >  ;
-LON(toDelete, :) = [];
-toDelete = LON<  ;
-LON(toDelete, :) = [];
+Wildfire_Locations_2020 = WildfireLocations2020;
+% LAT = WildfireLocations2020.latitude;
+% LON = WildfireLocations2020.longitude;
 
-%%
+%Removing wildfires that did not occur in boundaries of California
+%Border Range: 
+%   Longitude: 114.133333 W to 124.4 W
+%   Latitude: 32.5 N to 42 N
+% 
+% Latitude = LAT
+% Longitude = LON
+
+toDelete =Wildfire_Locations_2020.latitude > 42;
+Wildfire_Locations_2020(toDelete, :) = [];
+
+toDelete = Wildfire_Locations_2020.latitude < 32.5;
+Wildfire_Locations_2020(toDelete, :) = [];
+ 
+toDelete = Wildfire_Locations_2020.longitude < -124.4 ;
+Wildfire_Locations_2020(toDelete, :) = [];
+  
+toDelete = Wildfire_Locations_2020.longitude > -114.133333;
+Wildfire_Locations_2020(toDelete, :) = [];
+
+%Plotting 2020 Wildfire Data on a Map 
+latlim= [32.5 42]
+lonlim = [-124.4 -114.133333]
+
 figure (2)
-%worldmap([32 42], [246 236])
-cali = shaperead('usastatehi', 'UseGeoCoords', true,'Selector',{@(name) strcmpi(name,'California'), 'Name'});
-axesm mercator; %Define Map axes
-geoshow(cali); %Display map
-plotm(LAT,LON,'Color','r','Marker','.','MarkerSize',1); %plot point at [lat,lon] location
+ax= usamap(latlim,lonlim)
+axis off
+getm(gca, 'MapProjection')
+states = shaperead('usastatehi',...
+    'UseGeoCoords',true,'BoundingBox',[lonlim',latlim']);
+faceColors = makesymbolspec('Polygon',...
+    {'INDEX',[1 numel(states)],'FaceColor',polcmap(numel(states))});
+geoshow(ax,states,'SymbolSpec',faceColors)
+scatterm(Wildfire_Locations_2020.latitude,Wildfire_Locations_2020.longitude,5,'filled','k')
+title('Location of Wildfires in California in 2020')
 
-%%use worldmap and constrain the lat/long for california 
+
+
+% cali = shaperead('usastatehi', 'UseGeoCoords', true,'Selector',{@(name) strcmpi(name,'California'), 'Name'});
+% axesm mercator; %Define Map axes
+% geoshow(cali); %Display map
+% plotm(LAT,LON,'Color','r','Marker','.','MarkerSize',1); %plot point at [lat,lon] location
+% 
+% %%use worldmap and constrain the lat/long for california 

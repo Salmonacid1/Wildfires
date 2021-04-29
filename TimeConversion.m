517c85c85f1a95aaffd5fdc2d7ff0146250ee3a3
Salmonacid1 <78323292+Salmonacid1@users.noreply.github.com>
2021-04-29 17:35:11 -0400

    Work on Climate Divisions

--- TimeConversion.m
+++ TimeConversion.m
@@ -1,10 +1,16 @@
 function [CD_Adjusted_Time] = TimeConversion(filename)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%===================================================================
+%
+% USAGE:  [CD_Adjusted_Time] = TimeConversion(filename)
+%
+% DESCRIPTION:
+%   Use this function to convert the recorded times of the Climate Division
+%   Precipitation and Temperature data into a MATLAB recognized time. 
 
-Precipitation_CD = readtable(filename)
-Precip_Info = table2array(Precipitation_CD);
-CD_Date = Precip_Info(:,1)
+
+CD_Data = readtable(filename)
+CD_Info = table2array(CD_Data);
+CD_Date = CD_Info(:,1)
 Proper_Year = floor(CD_Date./100);
 Proper_Month = CD_Date -(Proper_Year .* 100); 
 CD_Adjusted_Time = datenum(Proper_Year, Proper_Month, 0);
